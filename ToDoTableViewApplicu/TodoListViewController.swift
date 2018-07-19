65dd632ddb1d94e5869643e95ef11162aee74af7
Noon <rodpwood@gmail.com>
2018-07-19 11:48:58 -0400

    add accessory options to cell

--- ToDoTableViewApplicu/TodoListViewController.swift
+++ ToDoTableViewApplicu/TodoListViewController.swift
@@ -33,6 +33,27 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    //Mark - TableView Delegate Methods
+    //look for method that creates an action when table view cell is selected
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         tableView.deselectRow(at: indexPath, animated: true)
+        
+       //if statement below adds and deletes the check mark as requested
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+    }
+    
+//    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .ishidden
+//
+//
+//    }
     //code below will create itemAray and place an element from the array in their own cell
     // in multiple sections being each count in itemArray will create a new section of individual cells
 //    override func numberOfSections(in tableView: UITableView) -> Int {
